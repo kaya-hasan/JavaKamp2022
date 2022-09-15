1a8a9a554e612af2b1b5e12170b6b06b0ba43cda
Hasan <58564918+kaya-hasan@users.noreply.github.com>
2022-09-15 19:08:15 +0300

    Add files via upload

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 12;
+        sayi = 129;
+        char karakter = 'A';
+        boolean dogruMu = false;
+    }
+}
