ab1d81c653a06084e09a8d1b2368366356078b4c
Hasan <hasankaya3546@hotmail.com>
2022-09-23 17:55:42 +0300

    2. Odev -> 30 -> ReCapDemo Classlarla Calismak

---
+++ reCapDemo_Classes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // hesap makinesi
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
